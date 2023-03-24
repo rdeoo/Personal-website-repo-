01fe211e6df88f0a089a6118e2bc3b4ff1c619f4
Rohan Deo <rohandeo307@gmail.com>
2023-03-24 13:48:58 -0700

    Javascript file created and linked. Created const array courseList with 3 items

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+const courseList = [
+  { code: "ACIT 1620", coursename: "Web Fundamental Technologies" },
+  { code: "COMM 1116", name: "Buisness Communications 1" },
+  { code: "ACIT 1515", name: "Scripting for IT" },
+]
+
+
