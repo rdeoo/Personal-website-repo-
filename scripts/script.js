786a19b1c456aa5b2e08f83c8373cba0870a7cdc
Rohan Deo <rohandeo307@gmail.com>
2023-03-24 23:03:16 -0700

    made for of loop to itterate over corselist to check if usercourse matched with code inside of array

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,27 @@
 const courseList = [
-  { code: "ACIT 1620", coursename: "Web Fundamental Technologies" },
+  { code: "ACIT 1620", name: "Web Fundamental Technologies" },
   { code: "COMM 1116", name: "Buisness Communications 1" },
   { code: "ACIT 1515", name: "Scripting for IT" },
 ];
 
-
-let validator = false
+let validator = false;
 let usercourse;
-while (!validator)
-{
+while (!validator) {
   usercourse = prompt("Please enter a 4 digit number - e.g. 1620: ");
-  if (usercourse.length === 4 && !isNaN(usercourse)){validator = true}
+  if (usercourse.length === 4 && !isNaN(usercourse)) {
+    validator = true;
+  }
 }
 
+let coursevalidator = false;
+for (let course of courseList) {
+  if (course.code.includes(usercourse)) {
+    console.log(`Yes i am taking the course: ${course.code} - ${course.name}`);
+    coursevalidator = true;
+    break;
+  }
+}
 
 // = prompt("Please enter a 4 digit number - e.g. 1620: ");
 // while (isNaN(usercourse) || usercourse.length !== 4) {
 //   prompt("Error. Please enter a number: ");
-
