15124bdca26aa3aaada6145a051b79012d31ca35
Rohan Deo <rohandeo307@gmail.com>
2023-03-24 22:37:49 -0700

    created prompt with validator that checks that the input is 4 digits and is a number not string or something or something else

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,19 @@ const courseList = [
   { code: "ACIT 1620", coursename: "Web Fundamental Technologies" },
   { code: "COMM 1116", name: "Buisness Communications 1" },
   { code: "ACIT 1515", name: "Scripting for IT" },
-]
+];
 
+
+let validator = false
+let usercourse;
+while (!validator)
+{
+  usercourse = prompt("Please enter a 4 digit number - e.g. 1620: ");
+  if (usercourse.length === 4 && !isNaN(usercourse)){validator = true}
+}
+
+
+// = prompt("Please enter a 4 digit number - e.g. 1620: ");
+// while (isNaN(usercourse) || usercourse.length !== 4) {
+//   prompt("Error. Please enter a number: ");
 
